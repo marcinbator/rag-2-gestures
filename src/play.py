ae0f03a08d1db0d83bf9a4343867c27676d015e0
Marcin Bator <marcinbator.ofc@gmail.com>
2025-01-27 16:23:24 +0100

    fix: #main threads import fixes

--- src/play.py
+++ src/play.py
@@ -5,7 +5,10 @@ import tornado.web
 import tornado.websocket
 
 from games.flappy.flappy_handler import FlappyGestures
+from games.flappy.models.flappy_gestures import flappy_start_gesture_recognition
 from games.happy.happy_handler import HappyGestures
+from games.happy.models.happy_gestures import happy_start_gesture_recognition
+from games.pong.models.pong_gestures import pong_start_gesture_recognition
 from games.pong.pong_handler import PongGestures, PongBot, PongRandom
 from games.skijump.models.skijump_gestures import skijump_start_gesture_recognition
 from games.skijump.skijump_handler import SkijumpGestures
@@ -22,13 +25,14 @@ socket_registry = [
 
 
 def launch_models():
-    # pong_gest_thread = threading.Thread(target=pong_start_gesture_recognition)
-    # flappy_gest_thread = threading.Thread(target=flappy_start_gesture_recognition)
-    # happy_gest_thread = threading.Thread(target=happy_start_gesture_recognition)
+    pong_gest_thread = threading.Thread(target=pong_start_gesture_recognition)
+    flappy_gest_thread = threading.Thread(target=flappy_start_gesture_recognition)
+    happy_gest_thread = threading.Thread(target=happy_start_gesture_recognition)
     skijump_gest_thread = threading.Thread(target=skijump_start_gesture_recognition)
 
     # pong_gest_thread.start()
     # flappy_gest_thread.start()
+    # happy_gest_thread.start()
     skijump_gest_thread.start()
 
 
